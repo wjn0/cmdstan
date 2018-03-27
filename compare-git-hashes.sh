de8bbfa4bb5254c1f619e2f34b6d23eb773eccc5
Sean Talts <sean.talts@gmail.com>
2018-03-27 15:23:03 -0400

    Better cleaning between runs

--- compare-git-hashes.sh
+++ compare-git-hashes.sh
@@ -5,7 +5,7 @@ usage() {
     echo "$0 <git-hash-1> <git-hash-2> <directories of models> '<extra args for runPerformanceTests.py>''"
 }
 
-checkout() {
+clean_checkout() {
     git checkout "$1"
     git submodule update --recursive
     dirty=$(git status --porcelain)
@@ -20,8 +20,11 @@ if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
     exit
 fi
 
-checkout "$1"
+clean_checkout "$1"
 ./runPerformanceTests.py -j8 --runj 5 --overwrite-golds $4 $3
 
-checkout "$2"
+git submodule foreach --recursive git clean -xffd
+make clean-all
+
+git checkout $2
 ./runPerformanceTests.py -j8 --runj 5 --check-golds-exact 1e-8 $4 $3
